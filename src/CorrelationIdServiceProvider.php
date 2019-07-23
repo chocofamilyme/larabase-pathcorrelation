ee07d3a7b18cb5c3d49b20b894c60ac031996a18
Kamenev <kamenev.n@gmail.com>
2019-07-23 12:29:06 +0500

    init

--- src/CorrelationIdServiceProvider.php
+++ src/CorrelationIdServiceProvider.php
@@ -15,24 +15,22 @@ class CorrelationIdServiceProvider extends BaseServiceProvider
      */
     public function register()
     {
+        //$this->app->register(CorrelationId::class);
         //$this->app->make('CorrelationId');
-        //$this->app->singleton(CorrelationId::class, function () {
-        //    return new CorrelationId();
-        //});
-
-        $this->app->register(CorrelationId::class);
+        $this->app->bind(CorrelationId::class, function ($app) {
+            return new CorrelationId();
+        });
     }
 
     /**
-     * Add the Cors middleware to the router.
      *
      */
     public function boot()
     {
-            /** @var \Illuminate\Foundation\Http\Kernel $kernel */
-           // $kernel = $this->app->make(Kernel::class);
-            //if (! $kernel->hasMiddleware(CorrelationId::class)) {
-            //    $kernel->prependMiddleware(CorrelationId::class);
-            //}
+        /** @var \Illuminate\Foundation\Http\Kernel $kernel */
+         $kernel = $this->app->make(Kernel::class);
+        if (! $kernel->hasMiddleware(CorrelationId::class)) {
+            $kernel->prependMiddleware(CorrelationId::class);
+        }
     }
 }
